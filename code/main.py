b5929c3c7938cb1f911f716832227e67feb31fc1
MichaelStroet <michael.stroet@hotmail.com>
2020-04-21 12:19:15 +0200

    messing with fits

--- code/main.py
+++ code/main.py
@@ -4,12 +4,16 @@ import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-root_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+from astropy.io import fits
+from fitsImage import *
+from matplotlib.colors import LogNorm
 
-# Add a path to the data folder
-sys.path.append(os.path.join(root_directory, "data"))
+root_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+data_directory = os.path.join(root_directory, "data")
+ALMA_directory = os.path.join(data_directory, "ALMA-HD100546")
 
 from modelImage import *
+from convolution import *
 
 if __name__ == "__main__":
 
@@ -20,8 +24,62 @@ if __name__ == "__main__":
     R_outer = 100 # AU
     pixelDimension = 500
 
-    image = getImageMatrix(frequency, inclination, R_inner, R_outer, pixelDimension)
-    saveImagePNG(image, pixelDimension, R_outer, f"image.png")
-    saveImageTXT(image, pixelDimension, R_outer, "image.txt")
+    # image = getImageMatrix(frequency, inclination, R_inner, R_outer, pixelDimension)
+    # saveImagePNG(image, pixelDimension, R_outer, f"image.png")
+    # saveImageTXT(image, pixelDimension, R_outer, "image.txt")
+    #
+    # plt.show()
+
+    ALMA_fits = os.listdir(ALMA_directory)
+
+    with fits.open(os.path.join(ALMA_directory, ALMA_fits[1])) as fits_file:
+        fits_file.info()
+
+        image = fits_file[0].data[0][0]
+        print()
+        print(image)
+        print(image.shape)
+        print()
+
+    sigma_x = 3
+    sigma_y = 2
+    theta = -(1/3) * np.pi
+
+    # image, (pixelDimension, pixelSize) = loadImageTXT("image.txt")
+
+    # print(f"Convolving image with sigx = {sigma_x}, sigy = {sigma_y}, theta = {theta}")
+    # convolved_image = convolveImageGaussian2D(image, sigma_x, sigma_y, theta)
+    # print("done\n")
+
+    plotFitsImage(image)
+
+    zerod_image = np.copy(image)
+    zerod_image[np.isnan(zerod_image)] = 0
+
+    profileRadius = 500
+
+    # from astropy.convolution import Gaussian2DKernel
+    # kernel = Gaussian2DKernel(sigma_x, sigma_y, -theta)
+    #
+    # plt.figure("2")
+    # ax1 = plt.subplot(1, 3, 1)
+    # ax1.imshow(image, cmap="inferno")
+    # ax1.set_title("Original")
+    # ax1.set_xlabel("X [pixels]")
+    # ax1.set_ylabel("Y [pixels]")
+    #
+    # ax2 = plt.subplot(1, 3, 2)
+    # ax2.imshow(kernel, cmap="viridis")
+    # ax2.set_title(f"Kernel")
+    # ax2.set_xlabel("X [pixels]")
+    # ax2.set_ylabel("Y [pixels]")
+    #
+    # ax3 = plt.subplot(1, 3, 3)
+    # ax3.imshow(convolved_image, cmap="inferno")
+    # ax3.set_title("Convolved")
+    # ax3.set_xlabel("X [pixels]")
+    # ax3.set_ylabel("Y [pixels]")
+
+    plotFitsRadialProfileWithImage(zerod_image, zerod_image.shape[0], profileRadius, theta)
 
     plt.show()
