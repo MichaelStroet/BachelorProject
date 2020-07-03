Gaussian model, variance bug fixed

code/mcmcFunctions.py
@@ -8,14 +8,28 @@ from astropy.convolution import convolve
 from radialProfileCircle import getCircleProfile
 from radialProfileEllipse import getEllipseProfile
 
-def getDataIntensities(data, radii, eccentricity, rotation):
+def getDataIntensities(convolved_data, radii, eccentricity, rotation, variance_range = None):
 
-    convolved_image = convolveDataImage(data)
-    data_intensities = np.asarray(getEllipseProfile(convolved_image, radii, eccentricity, rotation))
+    data_intensities = np.asarray(getEllipseProfile(convolved_data, radii, eccentricity, rotation))
     data_max = np.max(data_intensities)
 
-    # Return the intensities scaled in such a way that the peak is at 1
-    return data_intensities / data_max
+    if variance_range:
+
+        # Find indeces of the radii in the variance range
+        variance_intensities = []
+        for i, radius in enumerate(radii):
+            if variance_range[0] <= radius <= variance_range[1]:
+                variance_intensities.append(data_intensities[i])
+
+        variance = sum(variance_intensities) / len(variance_intensities)
+
+        # Return the intensities scaled in such a way that the peak is at 1 and
+        # the variance scaled by the same factor
+        return data_intensities / data_max, variance / data_max
+
+    else:
+        # Return the intensities scaled in such a way that the peak is at 1
+        return data_intensities / data_max
 
 def getModelIntensities(free_pars, pixel_coords, radii, arcsec_per_pix, sr_per_pix, fixed_pars, model_kernel, model, crop_radius, crop = False):
 
@@ -48,18 +62,16 @@ def logPrior(free_pars, pars_ranges):
 
 # log-likelihood function
 def logLikelihood(free_pars, constants):
-    pars_ranges, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius = constants
+    pars_ranges, variance, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius = constants
 
     I_model = getModelIntensities(free_pars, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, fixed_pars, model_kernel, model, crop_radius, True)
-    variance = np.sum((FIT_DATA_INTENSITIES - I_model)**2) / len(FIT_DATA_INTENSITIES)
-
     log_likelihood = -0.5 * np.sum(((FIT_DATA_INTENSITIES - I_model)**2 / variance) + np.log(2 * np.pi * variance))
 
     return log_likelihood
 
 # log-probability function
 def logProbability(free_pars, constants):
-    pars_ranges, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius = constants
+    pars_ranges, variance, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius = constants
 
     log_prior = logPrior(free_pars, pars_ranges)
     if np.isfinite(log_prior):

code/mcmcGaussian.py
@@ -21,18 +21,18 @@ from modelImage import *
 from convolution import *
 from mcmcFunctions import *
 
-def parameter_ranges(R1):
+def parameter_ranges():
 
     ranges = []
-    ranges.append([R1, 5])  # Rout (arcseconds)
-    ranges.append([0,  2])  # SigFrac = Sig2/Sig1
-    ranges.append([0,  3])  # p2
+    ranges.append([0.01, 0.20])  # Rin (arcseconds)
+    ranges.append([0.21, 1])     # Rout (arcseconds)
+    ranges.append([0, 5])       # c
 
     return ranges
 
 def mcmc(data, nwalkers, burnin_steps, production_steps):
     print("\n----------------------------------------------------------------------------------------------------\n")
-    print("   emcee - Markov chain Monte Carlo - Double model")
+    print("   emcee - Markov chain Monte Carlo - Gaussian model")
     print("\n----------------------------------------------------------------------------------------------------\n")
 
     ### Generate constants -----------------------------------------------------------------------------------------------
@@ -70,11 +70,7 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
 
     ### Setup model -------------------------------------------------------------------------------------------------------------
 
-    model = "double"
-
-    single_Rin = 0.11
-    single_Rout = 0.89
-    single_p = 0.81
+    model = "gaussian"
 
     # Fixed parameters
     v = 225e9 # Hz (219-235 GHz)
@@ -82,23 +78,21 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
     i = inclination # radian
     T0 = 30 # K
     q = 0.25
-    Sig1 = 0.25 # kg m^-2
-    Rin = single_Rin
-    R1 = single_Rout
-    p1 = single_p
+    Sig0 = 0.25 # kg m^-2
 
-    # Free parameters
-    Rout = 3
-    SigFrac = 1 # Sig2/Sig1
-    p2 = 1
+    # Free parameters guesses
+    Rin = 0.1 # Arcseconds
+    Rout = 0.5  # Arcseconds
+    c = 1
 
-    fixed_pars = (v, k, i, T0, q, Sig1, Rin, R1, p1)
-    free_pars = np.array([Rout, SigFrac, p2])
-    free_labels = ["Rout", "SigFrac", "p2"]
+    fixed_pars = (v, k, i, T0, q, Sig0)
+    free_pars = np.array([Rin, Rout, c])
+    free_labels = ["Rin", "Rout", "c"]
 
-    pars_ranges = parameter_ranges(R1)
+    pars_ranges = parameter_ranges()
 
     # Generate the convolution kernels for the model image
+    print(f"\nGenerating model kernels")
     model_kernel_area, model_kernel_peak = generateModelKernels(data)
 
     model_kernel = model_kernel_area
@@ -120,7 +114,6 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
         ### Burn-in sampler ---------------------------------------------------------------------------------------------------------
 
         start_locations = free_pars + (1e-3 * free_pars) * np.random.randn(nwalkers, ndim)
-
 
         print(f"\nRunning {burnin_steps} burn-in steps:")
         burnin_state = sampler.run_mcmc(start_locations, burnin_steps, progress = True)
@@ -157,7 +150,7 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
 
     # Create txt file with initial values
     with open(os.path.join(dir_path, "init.txt"), "a", encoding = "utf-8") as file:
-        parameter_txt = "Choices, constants and initial parameters\n"
+        parameter_txt = "Choices, constants an%d initial parameters\n"
         parameter_txt += f"\n"
         parameter_txt += f"Convolution kernel     {kernel_used} normalised\n"
         parameter_txt += f"Free parameters        {free_labels}\n"
@@ -175,15 +168,11 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
         parameter_txt += f"k                      {k} m^2/kg\n"
         parameter_txt += f"i                      {i} radian\n"
         parameter_txt += f"R in                   {Rin} arcseconds\n"
-        parameter_txt += f"R1                     {R1} arcseconds\n"
         parameter_txt += f"R out                  {Rout} arcseconds\n"
         parameter_txt += f"T0                     {T0} K\n"
         parameter_txt += f"q                      {q}\n"
-        parameter_txt += f"SigmaFrac              {SigFrac} Sig2/Sig1\n"
-        parameter_txt += f"Sigma1                 {Sig1} kg/m^2\n"
-        parameter_txt += f"Sigma2                 {SigFrac * Sig1} kg/m^2\n"
-        parameter_txt += f"p1                     {p1}\n"
-        parameter_txt += f"p2                     {p2}\n"
+        parameter_txt += f"Sigma0                 {Sig0} kg/m^2\n"
+        parameter_txt += f"c                      {c}\n"
         parameter_txt += f"\n"
         parameter_txt += f"Free parameters:\n{free_labels}\n"
         parameter_txt += f"Free parameter ranges:\n"

code/mcmcSingle.py
@@ -24,9 +24,9 @@ from mcmcFunctions import *
 def parameter_ranges():
 
     ranges = []
-    ranges.append([0.05, 0.15])  # Rin (arcseconds)
-    ranges.append([0.5, 1.5])     # Rout (arcseconds)
-    ranges.append([0.5, 1.5])       # p
+    ranges.append([0.01, 0.20])  # Rin (arcseconds)
+    ranges.append([0.21, 1])     # Rout (arcseconds)
+    ranges.append([0, 5])       # p
 
     return ranges
 
@@ -48,7 +48,7 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
     sr_per_pix = (data[1]["degreesPixelScale"] * np.pi / 180)**2
 
     fit_radius = 2 # Arcseconds
-    fit_radii = np.linspace(0, fit_radius, total_intensity_radii) # Arcseconds
+    fit_radii = np.linspace(0, fit_radius / arcsec_per_pix, total_intensity_radii) # pixels
 
     crop_radius = int(np.ceil((fit_radius + 0.5) / arcsec_per_pix)) # pixels
 
@@ -59,8 +59,14 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
     ### Get data intensity profile ----------------------------------------------------------------------------------------------
 
     print("\nGenerating data intensity profiles.")
-    FIT_DATA_INTENSITIES = getDataIntensities(data, fit_radii, eccentricity, rotation)
-    TOTAL_DATA_INTENSITIES = getDataIntensities(data, pixel_radii, eccentricity, rotation)
+    convolved_data = convolveDataImage(data)
+    variance_range = [1.25 / arcsec_per_pix, 1.75 / arcsec_per_pix]
+
+    FIT_DATA_INTENSITIES, variance = getDataIntensities(convolved_data, fit_radii, eccentricity, rotation, variance_range)
+    TOTAL_DATA_INTENSITIES = getDataIntensities(convolved_data, pixel_radii, eccentricity, rotation)
+
+    print(f"\nvariance_range: {variance_range}")
+    print(f"variance: {variance}")
 
     ### Setup model -------------------------------------------------------------------------------------------------------------
 
@@ -76,8 +82,8 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
 
     # Free parameters guesses
     Rin = 0.1 # Arcseconds
-    Rout = 1  # Arcseconds
-    p = 1
+    Rout = 0.5  # Arcseconds
+    p = 1.5
 
     fixed_pars = (v, k, i, T0, q, Sig0)
     free_pars = np.array([Rin, Rout, p])
@@ -98,7 +104,7 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
 
     ndim = len(free_pars)
 
-    CONSTANTS = (pars_ranges, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius)
+    CONSTANTS = (pars_ranges, variance, pixel_dimension, pixel_coords, fit_radii, arcsec_per_pix, sr_per_pix, FIT_DATA_INTENSITIES, fixed_pars, model_kernel, model, crop_radius)
 
     with Pool() as pool:
 
@@ -234,6 +240,11 @@ def mcmc(data, nwalkers, burnin_steps, production_steps):
     fig_name = "Images"
     fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2, 2, figsize = (12,12), num = fig_name)
 
+    ax1.imshow(data_image, origin="lower", norm=LogNorm(), cmap="inferno", extent = extent)
+    ax1.set_title("Original data")
+
+    ax2.imshow(model_image, origin="lower", norm=LogNorm(), cmap="inferno", extent = extent)
+    ax2.set_title("Original model (50th%)")
 
     ax3.imshow(convolved_data, origin="lower", norm=LogNorm(), cmap="inferno", extent = extent)
     ax3.set_title("Convolved data")

code/modelImage.py
@@ -32,11 +32,15 @@ def getImageMatrix(fixed_pars, free_pars, pixel_coords, arcsec_per_pix, sr_per_p
         Rin = fixed_pars[6]
         Rout = free_pars[0]
 
+    elif model == "gaussian":
+        from equations import thermalIntensityGaussian as thermalIntensity
+
+        Rin = free_pars[0]
+        Rout = free_pars[1]
+
     else:
         print(f"Error: Unknown model {model}")
         exit(1)
-
-
 
     matrix = np.zeros((len(pixel_coords), len(pixel_coords)))
 